c47aaa26732d22c43a0b6a54d1e7582b3044c3ae
大畑　陽平 <sunpeace1059@gmail.com>
2014-05-24 16:33:14 +0900

    Hello!!

---
+++ Hello!!.c
@@ -0,0 +1,4 @@
+﻿1>------ ビルド開始: プロジェクト:github, 構成:Debug Win32 ------
+1>  github.cpp
+1>  github.vcxproj -> C:\Users\vantan\Desktop\授業メモ\宗正\samples\github\Debug\github.exe
+========== ビルド: 1 正常終了、0 失敗、0 更新不要、0 スキップ ==========
